29f8eadd27f74e7c3a084f3e03350d49b980b2cc
mfan97 <71311372+mfan97@users.noreply.github.com>
2023-12-08 03:01:56 -0800

    adjusted backend

--- app.py
+++ app.py
@@ -1,26 +1,43 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 import openai
+import os
+from textblob import TextBlob
+from googletrans import Translator, LANGUAGES
 
 app = Flask(__name__)
 
 # Set up OpenAI API credentials
-<<<<<<< HEAD
+# <<<<<<< HEAD
 openai.api_key = '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'
-# shguieswbki
-# Define
+# Use environment variables or config files would store keys more securely.
+# But here just used the key directly for simplicity.
+
 # Define the default route to return the index.html file
-=======
-openai.api_key = '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' # Use environment variables or config files to store keys securely.
 
 # Configuration for SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://xiazhijie:Shinyway123!@localhost:5432/xiazhijie'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://xiazhijie:Shinyway123!@localhost:5432/xiazhijie'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://fanmuq:mypassword@localhost/chatdb'
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialize the database
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+# Initialize Google Translate
+translator = Translator()
+
+# Initialize Flask-Limiter
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    default_limits=["200 per day", "50 per hour"]  # We can always customize these values as needed
+)
+
 
 # Model to store messages and responses
 class ChatHistory(db.Model):
@@ -28,39 +45,56 @@ class ChatHistory(db.Model):
     user_message = db.Column(db.String(), nullable=False)
     ai_response = db.Column(db.String(), nullable=False)
 
->>>>>>> 655a3781b588c6be4de36300732bcfa841e8d4d0
+# >>>>>>> 655a3781b588c6be4de36300732bcfa841e8d4d0
 @app.route("/")
+@limiter.limit("10 per minute")  # Rate limit for this specific route
 def index():
     chat_history = ChatHistory.query.all()
     return render_template("index.html", chat_history=chat_history)
 
 @app.route("/api", methods=["POST"])
 def api():
-    message = request.json.get("message")
+    try:
+        data = request.json
+        message = data.get("message")
+        target_language = data.get("language", "en")  # Default to English if no language is provided
 
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "user", "content": message}
-        ]
-    )
+        # Translate message to English before sending to GPT-3
+        translated_message = translator.translate(message, dest='en').text
 
-<<<<<<< HEAD
-    else :
-        # failed to generaye
-=======
-    if completion.choices[0].message:
-        ai_response = completion.choices[0].message['content']
+         # Perform sentiment analysis on the message
+        analysis = TextBlob(message)
+        sentiment = analysis.sentiment
+        print(f"Sentiment Polarity: {sentiment.polarity}")  # For debugging
 
-        # Store the message and response in the database
-        chat_entry = ChatHistory(user_message=message, ai_response=ai_response)
+        # Modify the response based on the sentiment
+        if sentiment.polarity < -0.5:
+            ai_response = "I sense that you're upset. I'm here to help you. What can I do for you?"
+        else:
+            # For neutral and positive sentiment, proceed with translation and API call
+            completion = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[{"role": "user", "content": translated_message}]
+            )
+
+            if 'choices' in completion and completion.choices:
+                ai_response = completion.choices[0].message['content']
+            else:
+                ai_response = "I'm not sure how to respond to that."
+
+        # Translate AI response back to the target language
+        translated_response = translator.translate(ai_response, dest=target_language).text
+
+        # Store the original message and translated response in the database
+        chat_entry = ChatHistory(user_message=message, ai_response=translated_response)
         db.session.add(chat_entry)
         db.session.commit()
 
-        return ai_response
-    else:
->>>>>>> 655a3781b588c6be4de36300732bcfa841e8d4d0
-        return 'Failed to Generate response!'
+        return jsonify({"response": translated_response})
+
+    except Exception as e:
+        app.logger.error(f"Exception occurred: {e}")
+        return jsonify({"error": "Failed to generate response!"}), 500
 
 if __name__ == '__main__':
     app.run()
--- backend_test.py
+++ backend_test.py
@@ -1,34 +1,105 @@
-import pytest
+import unittest
 from app import app, db, ChatHistory
-from unittest.mock import patch
 
-@pytest.fixture
-def client():
-    app.config['TESTING'] = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'  # Use an in-memory database to avoid affecting the actual database
-    with app.test_client() as client:
+class ChatBotTestCase(unittest.TestCase):
+    # Set up before each test
+    def setUp(self):
+        # Configure the app for testing mode
+        app.config['TESTING'] = True
+        # Use an in-memory SQLite database for testing
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+        self.client = app.test_client()  # Create a test client
+
         with app.app_context():
-            db.create_all()  # Create test database tables
-        yield client
+            db.create_all()  # Create all database tables
+
+    # Tear down after each test
+    def tearDown(self):
+        # Clean up the database after each test
         with app.app_context():
-            db.drop_all()  # Drop test database tables after tests are complete
+            db.session.remove()
+            db.drop_all()
 
-def test_index_route(client):
-    response = client.get("/")
-    assert response.status_code == 200
-    assert "index.html" in response.data.decode()
+    # Test if the index page loads correctly
+    def test_index_page(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)  # Check if the response status code is 200
 
-@patch('app.openai.ChatCompletion.create')  # Mock the response from OpenAI API
-def test_api_route(mock_openai, client):
-    mock_response = {'choices': [{'message': {'content': 'Test response'}}]}
-    mock_openai.return_value = mock_response
+    # Test the API response for a standard message
+    def test_api_response(self):
+        test_message = {"message": "Hello", "language": "en"}
+        response = self.client.post('/api', json=test_message)
+        self.assertEqual(response.status_code, 200)  # Check if the response status code is 200
+        self.assertIn('response', response.json)  # Check if 'response' key is in the JSON response
 
-    response = client.post('/api', json={'message': 'Hello'})
-    assert response.status_code == 200
-    assert response.data.decode() == 'Test response'
+    # Test the API's sentiment analysis handling for a negative message
+    def test_api_sentiment_handling(self):
+        negative_message = {"message": "I am very unhappy and angry", "language": "en"}
+        response = self.client.post('/api', json=negative_message)
+        self.assertEqual(response.status_code, 200)  # Check if the response status code is 200
+        # Check if the API response includes the expected sentiment acknowledgment
+        self.assertIn("I sense that you're upset", response.json['response'])
 
-    # Test if the database correctly stored the data
-    with app.app_context():
-        chat_entry = ChatHistory.query.first()
-        assert chat_entry.user_message == 'Hello'
-        assert chat_entry.ai_response == 'Test response'
+    # Test if a database entry is created after sending a message
+    def test_database_entry_creation(self):
+        message = {"message": "Testing database entry", "language": "en"}
+        self.client.post('/api', json=message)
+
+        with app.app_context():
+            # Query the first entry in the ChatHistory table
+            entry = ChatHistory.query.first()
+            self.assertIsNotNone(entry)  # Check if an entry was created
+            # Check if the message in the database matches the one sent
+            self.assertEqual(entry.user_message, message['message'])
+    # ... [previous test cases]
+
+    # Test the API's response to invalid data
+    def test_api_with_invalid_data(self):
+        # Send an empty message to the API
+        invalid_data = {"message": "", "language": "en"}
+        response = self.client.post('/api', json=invalid_data)
+        # Expect a 400 Bad Request status code due to invalid input
+        self.assertEqual(response.status_code, 400)
+        # Check if 'error' key is present in the JSON response
+        self.assertIn('error', response.json)
+
+    # Test the API's handling of non-English languages
+    def test_api_with_non_english_language(self):
+        # Send a message in Spanish
+        non_english_message = {"message": "Hola, ¿cómo estás?", "language": "es"}
+        response = self.client.post('/api', json=non_english_message)
+        # Expect a successful response
+        self.assertEqual(response.status_code, 200)
+        # Check if 'response' key is in the JSON response
+        self.assertIn('response', response.json)
+
+    # Test the API's rate limiting feature
+    def test_api_rate_limiting(self):
+        # Send multiple requests to test rate limiting
+        message = {"message": "Test rate limiting", "language": "en"}
+        for _ in range(15):  # Assuming a rate limit is set, exceed it
+            response = self.client.post('/api', json=message)
+        # Expect a non-200 status code (e.g., 429 Too Many Requests) when rate limit is exceeded
+        self.assertNotEqual(response.status_code, 200)
+
+    # Test if multiple database entries are created correctly
+    def test_database_multiple_entries(self):
+        # Send multiple distinct messages
+        messages = [
+            {"message": "First message", "language": "en"},
+            {"message": "Second message", "language": "en"}
+        ]
+        for msg in messages:
+            self.client.post('/api', json=msg)
+
+        with app.app_context():
+            # Count the number of entries in the database
+            entries = ChatHistory.query.count()
+            # Check if the count matches the number of messages sent
+            self.assertEqual(entries, len(messages))
+
+
+if __name__ == '__main__':
+    unittest.main()
